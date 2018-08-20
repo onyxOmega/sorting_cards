c36b917d6b5d6d9654f109745ab4eefa86018226
William Fischer <william.fischer42@gmail.com>
2018-08-20 17:36:19 -0600

    Added deck test and card ranking parameter

--- lib/card.rb
+++ lib/card.rb
@@ -1,7 +1,28 @@
 class Card
-  attr_reader :value, :suit
+  attr_reader :value, :suit, :rank
   def initialize(value, suit)
     @value = value
     @suit = suit
+    @rank = value_rank + suit_rank
+    require 'pry'; binding.pry
   end
+
+  def value_rank
+    if value.to_i != nil
+      return value.to_i * 10
+    else
+      return 110 if @value == "Jack"
+      return 120 if @value == "King"
+      return 130 if @value == "Queen"
+      return 140 if @value == "Ace"
+    end
+  end
+
+  def suit_rank
+    return 1 if @suit == "Clubs"
+    return 2 if @suit == "Diamonds"
+    return 3 if @suit == "Hearts"
+    return 4 if @suit == "Spades"
+  end
+
 end
--- lib/deck.rb
+++ lib/deck.rb
@@ -9,4 +9,8 @@ class Deck
   def count
     @cards.length
   end
+
+  def sort
+    
+  end
 end
